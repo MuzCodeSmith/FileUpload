5a113eb76c1a718f9d086c51827d16d055993bf4
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-01-28 22:02:25 +0530

    installed necessory packages for file upload and updated index file

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 require('dotenv').config()
-require('./config/database').connectDB();
-const app = express();
 
+
+const app = express();
 const PORT = process.env.PORT || 3000;
+const fileupload = require('express-fileupload')
 
 // middlewares
 app.use(express.json());
+app.use(fileupload())
+
+// database connection
+require('./config/database').connectDB();
+
+// cloudinary connection
+require('./config/cloudinary').cloudinaryConnect();
+
+// mounting routes
+const upload = require('./routes/FIleUpload')
+app.use('/api/v1/upload',upload)
 
 // default route
 app.get('/',(req,res)=>{
