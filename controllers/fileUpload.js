a5e893cb98cb29ef100753ba17701358f0006eb8
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-01-29 21:38:59 +0530

    added imageUpload controller

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -1,4 +1,5 @@
 const File = require('../models/File');
+const cloudinary = require('cloudinary').v2;
 
 exports.localFileUpload = async (req,res)=>{
     try {
@@ -16,6 +17,48 @@ exports.localFileUpload = async (req,res)=>{
             message:'file uploaded successfully'
         })
     } catch (error) {
+        res.status(500).json({
+            success:false,
+            error:error.message,
+        }) 
+    }
+}
+
+const isSupportedFormat = (type, supportedFormats) =>{
+    return supportedFormats.includes(type)
+}
+
+const uploadFileToCloudinary = async (file,folder) =>{
+    const options = {folder}
+    await cloudinary.uploader.upload(file.tempFilePath,options)
+}
+
+exports.imageUpload = async (req,res)=>{
+    try {
+        const {name, tags, email} = req.body;
+        console.log(name, tags, email)
+
+        const file = req.files.imageFile;
+        console.log("file:",file);
+
+        const supportedFormats = ['jpg','jpeg','png'];
+        const fileType = file.name.split('.')[1].toLowerCase();
         
+        if(!isSupportedFormat(fileType,supportedFormats)){
+            return res.status(500).json({
+                success:false,
+                message:"file format not supported!"
+            }) 
+        }
+
+        const response = await uploadFileToCloudinary(file, 'muzzu')
+
+
+
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            error:error.message,
+        }) 
     }
 }
--- routes/FIleUpload.js
+++ routes/FIleUpload.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const {localFileUpload} = require('../controllers/fileUpload')
+const {localFileUpload, imageUpload} = require('../controllers/fileUpload')
 
 router.post('/localFileUpload',localFileUpload)
+router.post('/imageUpload',imageUpload)
 
 module.exports = router;
