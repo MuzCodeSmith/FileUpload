78ceef3e801c533876cc1549409c229f194cad1d
MuzCodeSmith <shaikhmuzffar82@gmail.com>
2025-01-30 22:34:44 +0530

    add compressedImageUpload controller

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -31,10 +31,14 @@ const isSupportedFormat = (type, supportedFormats) =>{
     return supportedFormats.includes(type)
 }
 
-async function uploadFileToCloudinary(file,folder){
+async function uploadFileToCloudinary(file,folder,quality){
     const options = {folder}
     console.log("file:",file)
     options.resource_type ="auto";
+
+    if(quality){
+        options.quality = quality; 
+    }
     return await cloudinary.uploader.upload(file.tempFilePath,options)
 }
 
@@ -85,6 +89,47 @@ exports.videoUpload = async (req,res) =>{
         const {email,name,tags} = req.body;
         const file = req.files.videoFile;
 
+
+        const supportedFormats = ['mov','mp4'];
+        const fileType = file.name.split('.')[1].toLowerCase();
+
+        if(!isSupportedFormat(fileType,supportedFormats)){
+            return res.status(400).json({
+                success:false,
+                message:"File format not supported"
+            })
+        }
+
+        const response = await uploadFileToCloudinary(file, 'muzzu')
+
+
+        const fileData = await File.create({
+            tags,
+            email,
+            name,
+            imageUrl:response.secure_url
+        })
+
+        res.json({
+            success:true,
+            imageUrl:response.secure_url,
+            message:"Video Uplaoded Successfully"
+        })
+        
+    }catch (error) {
+            res.status(555).json({
+                success:false,
+                error:error.message,
+        }) 
+    }
+}
+
+exports.compressedImageUpload = async (req,res) =>{
+    try {
+
+        const {email,name,tags} = req.body;
+        const file = req.files.videoFile;
+
         console.log("file: ",file)
 
         const supportedFormats = ['mov','mp4'];
@@ -98,7 +143,7 @@ exports.videoUpload = async (req,res) =>{
             })
         }
 
-        const response = await uploadFileToCloudinary(file, 'muzzu')
+        const response = await uploadFileToCloudinary(file, 'muzzu',30)
 
         console.log("response:",response)
 
@@ -112,7 +157,7 @@ exports.videoUpload = async (req,res) =>{
         res.json({
             success:true,
             imageUrl:response.secure_url,
-            message:"Video Uplaoded Successfully"
+            message:"Compressed Image Uplaoded Successfully"
         })
         
     }catch (error) {
--- routes/FIleUpload.js
+++ routes/FIleUpload.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const {localFileUpload, imageUpload, videoUpload} = require('../controllers/fileUpload')
+const {localFileUpload, imageUpload, videoUpload, compressedImageUpload} = require('../controllers/fileUpload')
 
 router.post('/localFileUpload',localFileUpload)
 router.post('/imageUpload',imageUpload)
 router.post('/videoUpload',videoUpload)
+router.post('/compressedImageUpload',compressedImageUpload)
 
 module.exports = router;
